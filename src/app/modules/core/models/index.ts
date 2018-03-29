356740df20a0541f49d0119d3af6b9f3fecd6104
AmraniO <o.amrani@gmail.com>
2018-03-29 21:58:29 +0200

    refa: delete Meta model and consider it as any

--- src/app/modules/core/models/index.ts
+++ src/app/modules/core/models/index.ts
@@ -2,7 +2,6 @@ export * from "./entity.model";
 export * from "./panel.model";
 export * from "./section.model";
 export * from "./control.model";
-export * from "./meta.model";
 export * from "./field.model";
 export * from "./option.model";
 export * from "./lov.model";
