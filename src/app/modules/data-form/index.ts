762dbf76bc6fb074f42f44efccff4a8636a696ab
AmraniO <o.amrani@gmail.com>
2018-03-29 21:54:35 +0200

    chore(data-form): add animations

--- src/app/modules/data-form/index.ts
+++ src/app/modules/data-form/index.ts
@@ -7,7 +7,7 @@ import {
   MatSelectModule,
   MatButtonModule
 } from "@angular/material";
-import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { ReactiveFormsModule } from "@angular/forms";
 
 import { DataFormComponent } from "./components/data-form.component";
@@ -22,7 +22,7 @@ import { SortPipe } from "../core/pipes";
     MatDatepickerModule,
     MatSelectModule,
     MatButtonModule,
-    NoopAnimationsModule,
+    BrowserAnimationsModule,
     ReactiveFormsModule
   ],
   entryComponents: [DataFormComponent],
