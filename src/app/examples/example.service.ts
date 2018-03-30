a84c9679c00b9ae3ce66f7d4a86c4cc5f9af9d4a
AmraniO <o.amrani@gmail.com>
2018-03-30 22:22:15 +0200

    feat(service): implement post and put

--- src/app/examples/example.service.ts
+++ src/app/examples/example.service.ts
@@ -1,22 +1,25 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Observable } from "rxjs/Observable";
 
 const API: string = "http://localhost:51203/odata/";
+const OPTIONS = {
+  headers: new HttpHeaders().set("Content-Type", "application/json")
+};
 
 @Injectable()
 export class ExampleService {
   constructor(private httpClient: HttpClient) {}
 
-  // getMeta(): Observable<any> {
-  //   return this.httpClient.get(`${url}meta.json`);
-  // }
-
   getData(url: string): Observable<any> {
     return this.httpClient.get(`${API}${url}`);
   }
 
-  // getDataForm(): Observable<any> {
-  //   return this.httpClient.get(`${url}dataForm.json`);
-  // }
+  addItem(url: string, payload: any): Observable<any> {
+    return this.httpClient.post(`${API}${url}`, payload, OPTIONS);
+  }
+
+  updateItem(url: string, payload: any): Observable<any> {
+    return this.httpClient.put(`${API}${url}`, payload, OPTIONS);
+  }
 }
