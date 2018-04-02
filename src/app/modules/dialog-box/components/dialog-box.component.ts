aa826f2a8580c4ee98bae6eed136368931e02f2f
AmraniO <o.amrani@gmail.com>
2018-04-02 14:48:07 +0200

    fix(dialog-box): fix 'color' property

--- src/app/modules/dialog-box/components/dialog-box.component.ts
+++ src/app/modules/dialog-box/components/dialog-box.component.ts
@@ -1,12 +1,15 @@
 import { Component, Inject } from "@angular/core";
 import { MatDialogRef } from "@angular/material";
-import { MAT_DIALOG_DATA } from '@angular/material';
+import { MAT_DIALOG_DATA } from "@angular/material";
 
 @Component({
-    selector: "nui-dialog-box",
-    templateUrl: "./dialog-box.component.html",
-    styleUrls: ["./dialog-box.component.scss"]
+  selector: "nui-dialog-box",
+  templateUrl: "./dialog-box.component.html",
+  styleUrls: ["./dialog-box.component.scss"]
 })
 export class DialogBoxComponent {
-    constructor(public dialogRef: MatDialogRef<DialogBoxComponent>, @Inject(MAT_DIALOG_DATA) public data: any) { }
+  constructor(
+    public dialogRef: MatDialogRef<DialogBoxComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
 }
