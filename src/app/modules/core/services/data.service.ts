3c7b8fda65a91ce71e21197ae22d95cc2115520b
AmraniO <o.amrani@gmail.com>
2018-04-02 17:08:23 +0200

    feat(service): implement new data service

--- src/app/modules/core/services/data.service.ts
+++ src/app/modules/core/services/data.service.ts
@@ -7,28 +7,27 @@ import {
 import { Observable } from "rxjs/Observable";
 import { of } from "rxjs/observable/of";
 
-const API: string = "http://localhost:51203/odata/";
 const OPTIONS = {
   headers: new HttpHeaders().set("Content-Type", "application/json")
 };
 
 @Injectable()
-export class ExampleService {
+export class DataService {
   constructor(private httpClient: HttpClient) {}
 
   getData(url: string): Observable<any> {
-    return this.httpClient.get(`${API}${url}`);
+    return this.httpClient.get(`${url}`);
   }
 
   addItem(url: string, payload: any): Observable<any> {
-    return this.httpClient.post(`${API}${url}`, payload, OPTIONS);
+    return this.httpClient.post(`${url}`, payload, OPTIONS);
   }
 
   updateItem(url: string, payload: any): Observable<any> {
-    return this.httpClient.put(`${API}${url}`, payload, OPTIONS);
+    return this.httpClient.put(`${url}`, payload, OPTIONS);
   }
 
   deleteItem(url: string): Observable<any> {
-    return this.httpClient.delete(`${API}${url}`, OPTIONS);
+    return this.httpClient.delete(`${url}`, OPTIONS);
   }
 }
--- src/app/modules/core/services/index.ts
+++ src/app/modules/core/services/index.ts
@@ -1,3 +1,4 @@
 export * from "./action.service";
 export * from "./dialog.service";
 export * from "./common.service";
+export * from "./data.service";
